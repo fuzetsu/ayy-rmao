5155a4d0fa0867eb32715b4846518d8666cc78bf
Daniel Loomer <fuzetsu@users.noreply.github.com>
2018-04-28 23:24:43 -0400

    fix gfycat jsonp request

--- js/main.js
+++ js/main.js
@@ -241,9 +241,7 @@
       return url.replace(/\/gallery\//, '/a/').replace(/^http:/, 'https:') + '/embed?pub=true&analytics=false';
     }},
     { type: 'Video', match: /gfycat\.com\/[a-z0-9]+$/i, strip: true, parseAsync: function(url) {
-      return m.request({
-        method: 'GET',
-        dataType: 'jsonp',
+      return m.jsonp({
         url: 'http://gfycat.com/cajax/get/' + url.match(/gfycat\.com\/([a-z0-9]+)$/i)[1],
         background: true
       }).then(function(data) {
