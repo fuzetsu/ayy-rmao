532b8a612eb57bb6624cdd3bf15190637db46803
Daniel Loomer <fuzetsu@users.noreply.github.com>
2019-06-07 00:46:40 -0400

    make zaftig use module

--- js/ext-deps.js
+++ js/ext-deps.js
@@ -1,4 +1,4 @@
-import z from 'https://unpkg.com/zaftig'
+import z from 'https://unpkg.com/zaftig?module'
 z.setDebug(true)
 export { z }
 export { default as m } from 'https://unpkg.com/mithril@2.0.0-rc.4/mithril.mjs'
