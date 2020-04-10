95df693a0e0fa70f76a145dc8ca882a53daedaf7
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-04-09 23:57:12 -0400

    update deps

--- js/ext-deps.js
+++ js/ext-deps.js
@@ -2,7 +2,7 @@ export { default as m } from 'https://unpkg.com/mithril@2.0.0-rc.4/mithril.mjs?m
 
 export { default as merge } from 'https://unpkg.com/mergerino@0.4.0/dist/mergerino.min.js?module'
 
-export { default as z } from 'https://unpkg.com/zaftig@0.8.1/dist/zaftig.min.js?module'
+export { default as z } from 'https://unpkg.com/zaftig@0.8.4/dist/zaftig.min.js?module'
 
 export { default as setupMeiosis } from 'https://unpkg.com/meiosis-setup@2.0.0/mergerino/index.js?module'
 
