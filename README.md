8e65caea9572781fc66202dd7b83c59f10abeccd
fuzetsu <fuzetsu@users.noreply.github.com>
2020-12-11 03:14:31 +0000

    deploy: 94b68b1c8c59c1e6e4ac209cf85855d255bdd58c

--- README.md
+++
@@ -1,3 +0,0 @@
-# Ayy RMAO! (Reddit my ass off)
-
-A simple image driven view of Reddit. [Check it out!](http://fuzetsu.github.com/ayy-rmao).
--- src/lib/view-util.js
+++
@@ -1,6 +0,0 @@
-import { m, z } from '/vdom'
-import LOADING_IMG from '/img/loading.gif'
-
-export const loadingImg = () => m('img' + z`filter invert($loading-filter)`, { src: LOADING_IMG })
-
-export const externalLink = 'a[target=_blank][rel=nofollow noreferrer noopener]'
